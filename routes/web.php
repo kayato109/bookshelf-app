c970b5b23e40c97b947c83e6c93c5b5a6534ce13
Takato Kamikiya <kaosu763@gmail.com>
2026-05-18 14:13:11 +0900

    feat: 認証機能の実装（Fortify） #4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\RankingController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\GenreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [BookController::class, 'index']);
+
+// 書籍一覧（Fortify実装時にとりあえず追加）
+Route::get('/books', [BookController::class, 'index'])
+    ->name('books.index');
+
+// 書籍登録画面（Fortify実装時にとりあえず追加）
+Route::get('/books/create', [BookController::class, 'create'])
+    ->middleware('auth')
+    ->name('books.create');
+
+// 書籍詳細（Fortify実装時にとりあえず追加）
+Route::get('/books/{book}', [BookController::class, 'show'])
+    ->name('books.show');
+
+// ランキング（Fortify実装時にとりあえず追加）
+Route::get('/ranking', [RankingController::class, 'index'])
+    ->name('ranking.index');
+
+// お気に入り一覧（Fortify実装時にとりあえず追加）
+Route::get('/favorites', [FavoriteController::class, 'index'])
+    ->middleware('auth')
+    ->name('favorites.index');
+
+// ジャンル一覧（Fortify実装時にとりあえず追加）
+Route::get('/genres', [GenreController::class, 'index'])
+    ->middleware('auth')
+    ->name('genres.index');
+
+//ルートが見つからない場合リダイレクト(最後に記述のこと)
+Route::fallback(function () {
+    return redirect('/');
 });
