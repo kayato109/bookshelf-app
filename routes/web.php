b22f2dfb387c972e3046a7d47892abec2ad26725
Takato Kamikiya <kaosu763@gmail.com>
2026-05-19 15:27:45 +0900

    feat: 書籍登録機能 #7

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::middleware('auth')->group(function () {
 
     //書籍作成
     Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+    Route::post('/books', [BookController::class, 'store'])->name('books.store');
 
     // 書籍編集
     Route::get('/books/{book}/edit', [BookController::class, 'edit'])
