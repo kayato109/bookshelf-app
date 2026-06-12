150eee58474905a029242b0afb5ff42f6010421a
Takato Kamikiya <kaosu763@gmail.com>
2026-06-12 15:10:08 +0900

    feat: 応用要件：読書計画の更新 #51

---
+++ app/Http/Requests/UpdateReadingPlanRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateReadingPlanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'target_date' => [
+                'required',
+                'date',
+                'after_or_equal:today',
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'target_date.required' => '期日を入力してください',
+            'target_date.after_or_equal' => '期日は今日以降の日付を入力してください',
+        ];
+    }
+}
